606be740ab3f6584828af000aa30b3fc9efe709a
ndtam <ndtam146@gmail.com>
2017-11-09 15:52:01 +0700

    hoan thanh nhanh admin-catalogs

---
+++ app/admin/admin-catalogs/index.php
@@ -0,0 +1,51 @@
+<?php require_once "../../../db/mysql.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Danh sach nguoi dung</title>
+   <link rel="stylesheet" type="text/css" href="../../../public/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../public/css/custom.css">
+</head> 
+<body>
+  <div class="container">
+  <h2>Danh sach nguoi dung</h2>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php
+    $sql = "select * from catalogs";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) { ?>
+        <tr>
+          <td><?php echo $row['name']; ?></td>
+          <td><?php echo $row['description']; ?></td>
+          <!-- <td><?php switch ($row['role']) {
+            case 0:
+              echo "Admin";
+              break;
+            case 1:
+              echo "Editor";
+              break;
+            case 2:
+              echo "User";
+              break;
+          }; ?>            
+          </td> -->
+
+          <td><a href="edit.php?id=<?php echo $row['id'];?>">Edit</a></td>
+          <td><a href="delete.php?id=<?php echo $row['id'];?>">Delete</a></td>
+        </tr>
+    <?php  }
+    }
+     ?>       
+    </tbody>
+  </table>
+</div>
+</body>
+</html>
