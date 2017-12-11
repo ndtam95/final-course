af1d681b62a3c2361e7dc837213c5b64d3ae7318
ndtam <ndtam146@gmail.com>
2017-12-11 17:04:54 +0700

    fafasf

---
+++ app/about.php
@@ -0,0 +1,89 @@
+<!-- <?php
+  define("URL_IMAGE", "http://localhost:81/php/final-course/public/uploads/");
+?>
+ --><!DOCTYPE html>
+<html lang="en">
+  <head>    
+    <?php include "lib.php"; ?>    
+  </head>
+  <body>
+
+    <!-- Navigation -->
+    <?php include "menu.php" ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        
+        <!-- /.col-lg-3 -->
+        <div class="col-lg-12">
+        <?php include "slide.php" ?> 
+          
+          <!-- /.row -->
+
+        </div>
+        <!-- /.col-lg-9 -->      
+
+      </div>
+      <!-- /.row -->
+      <div class="row">
+     	<div class="txtabout">
+     		<h2><b>Marketing là gì? Định nghĩa marketing của Philip Kotler chuẩn nhất</b></h2>
+     		<p>Có  rất nhiều định nghĩa về Marketing nhưng chuẩn nhất vẫn là theo ngài Philip Kotler – Mệnh danh là cha đẻ của ngành Marketing hiện đại định nghĩa thì Marketing :
+
+			Marketing là gì : Nghệ thuật tạo ra giá trị, truyền thông và phân phối những giá trị đó nhằm thỏa mãn vấn đề của khách hàng mục tiêu để đem lại lợi nhuận tối ưu cho doanh nghiệp.
+			Nguyên bản định nghĩa tiếng Anh Marketing là gì : “the science and art of exploring, creating, and delivering value to satisfy the needs of a target market at a profit”
+
+			Như vậy Marketing được gọi là cuộc chiến về giá trị chứ không phải là bán sản phẩm mà doanh nghiệp có. Về giá trị có 10 loại giá trị mà theo mr Tuấn Hà thường gọi là : Thập Đại Lợi Ích trong các lớp học về marketing tại Vinalink</p>
+			<img src="../public/uploads/img1ab.png">
+			<h3>Hãy xem 10 loại giá trị đó là gì?</h3>
+			<ul>
+				<li>Giá trị Lý tính cơ bản : Giải quyết các vấn đề mà khách hàng đang cần.</li>
+				<li>Giá trị cấp bách, thời vụ, thời tiết, ngữ cảnh xã hội, cảm xúc trạng thái, Tò mò dùng thử : Giải quyết nhu cầu cấp bách</li>
+				<li>Giá trị niềm tin : Do thương hiệu đó làm tốt nhận diện và định vị khác biệt, Niềm tin về xuất xứ, về đòn bẩy do khách hàng cũ và các loại đòn bẩy khác</li>
+				<li>Giá trị cảm nhận , so sánh, muốn khám phá : Do giác quan và sản phẩm tương tự đem lại sự so sánh, ví dụ như bao bì đẹp, sang trọng, Nơi phân phối đẳng cấp, sản phẩm sờ chắc tay tinh xảo….</li>
+				<li>Giá trị cảm tính phe phái, bầy đàn, xếp phía : Được coi là loại xếp hạng nào (phân khúc)</li>
+				<li>Giá trị cảm tính giải quyết lo ngại, sự mất mát, đe dọa và sản phẩm sẽ đáp ứng được.</li>
+				<li>Giá trị cảm tính tương lai, lợi ích tương lai – chi phí cơ hội.</li>
+				<li>Giá trị cảm tính cái tôi, sự tôn trọng,  triết lý sống : Thể hiện bản thân và sự tôn trọng, giá trị tinh thần, văn hóa, trách nhiệm xã hội….</li>
+				<li>Giá trị lý tính gia tăng : Quà tặng…</li>
+				<li>Giá trị liên tưởng gia tăng : Các giá trị mang tính liên tưởng, tỉnh cảm thương hiệu (Love mark), tình thân đồng loại</li> 
+			</ul>
+			<h3>Quy trình Marketing của Kotler cũng đưa ra các bước : RSTPMMIC (Rồi sẽ Tự Phải Mò Mẫm I Chút)</h3>
+			<p>
+				Bước 1 : Research : Nghiên cứu thị trường <br>
+				Bước 2 : Segmentation : Phân khúc khách hàng theo khả năng chi trả, họ ở đâu, lối sống, độ tuổi, giới tính… <br>
+				Bước 3 : Target Market : Phân khúc xong thì chọn Thị trường mục tiêu : Khi nào, bao giờ , địa điểm, phương pháp phân phối… <br>
+				Bước 4 : Positioning : Định vị thương hiệu/ phương pháp cạnh tranh khác biệt. <br>
+				Bước 5 : Marketing Mix : bao gồm 4P hay 7P : Sản phẩm gì, Phân phối ra sao, Phương pháp định giá bán, Quảng bá truyền thông, Con người, Cơ sở vật chất, quy trình… <br>
+				Bước 6 : Implementation : Thực thi tạo sản phẩm, phân phối, lập Media plan, thực thi marketing, truyền thông… <br>
+				Bước 7 : Control : Kiểm soát KPI, chi phí marketing/ doanh thu
+			</p>
+			<h3>Về bản chất cơ bản của Marketing mix là 4P và các biến thể của nó</h3>
+			<p>
+				Product : Sản phẩm gì? Bao bì ra sao? <br>
+				Price : Giá bao nhiêu? <br>
+				Place : Phương pháp phân phối sản phẩm – Trade marketing <br>
+				Promotion : Quảng bá hình ảnh, thương hiệu ra sao : Nhận diện, định vị, bán hàng… <br>
+				People : Con người ra sao, nhân lực tay nghề cao, thái độ tốt, trách nhiệm thừa…. <br>
+				Process : Quy trình bán hàng chuyên nghiệp <br>
+				Phisical Evidents : Cơ sở vật chất đẹp, rộng, khang trang, độ phủ tốt…
+			</p>
+     	</div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <?php include "footer.php" ?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="../public/js/jquery.min.js"></script>
+    <script src="../public/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
+
--- app/index.php
+++ app/index.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 <?php require_once "../db/mysql.php"; ?>
 <?php
-  define("URL_IMAGE", "http://localhost:81/php/final-course/public/uploads/"); 
+  define("URL_IMAGE", "http://localhost:81/php/final-course/public/uploads/");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,54 +9,107 @@
     <?php include "lib.php"; ?>
   </head>
   <body>
-
+<?php if(isset($_SESSION["flash"])) echo $_SESSION["flash"]; ?>
     <!-- Navigation -->
     <?php include "menu.php" ?>
-
-    <!-- Page Content -->
+       <!-- Page Content -->
     <div class="container">
-      <div class="row">
-        <i class="flash"><?php if(isset($_SESSION["flash"])) echo $_SESSION["flash"]; ?></i>
-      </div>
-      <div class="row">
+         <div class="row">
+          <?php include "slide-left.php" ?>
+            <div class="col-md-9"><?php include "slide.php" ?></div>
 
-        <?php include "slide-left.php" ?>
+            
+         </div>
+      <div class="row">
         <!-- /.col-lg-3 -->
-        <div class="col-lg-9">
-          <?php include "slide.php" ?>
+        <div class="col-lg-12 ndchinh my-3">
+
+          
+          
           <div class="row">
             <?php
-                $sql = "select * from products";
-                $result = $conn->query($sql);
-                if($result->num_rows > 0)
-                {
-                  while($row = $result->fetch_assoc()) {?>
-                  <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="card h-100">
-                      <a href="single.php?products_id=<?php echo $row["id"];?>"><img class="card-img-top" src="<?php echo URL_IMAGE.$row['image'];?>" alt=""></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
-                          <a href="single.php?products_id=<?php echo $row["id"];?>">
-                            <span style="display:block;text-overflow: ellipsis;width: 200px;overflow: hidden; white-space: nowrap;">
-                              <?php echo $row["name"];?>
-                            </span></a>
-                        </h4>
-                        <h5><?php echo $row["price"]?> VNĐ</h5>                        
-                        <p class="card-text"><?php echo $row["description"];?>.</p>
-                      </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                      </div>
-                    </div>
+                // $sql = "select * from products";
+                // $result = $conn->query($sql);
+            // BƯỚC 2: TÌM TỔNG SỐ RECORDS
+            $result = mysqli_query($conn, 'select count(id) as total from products');
+            $row = mysqli_fetch_assoc($result);
+            $total_records = $row['total'];
+
+            // BƯỚC 3: TÌM LIMIT VÀ CURRENT_PAGE
+            $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+            $limit = 12;
+
+            // BƯỚC 4: TÍNH TOÁN TOTAL_PAGE VÀ START
+            // tổng số trang
+            $total_page = ceil($total_records / $limit);
+
+            // Giới hạn current_page trong khoảng 1 đến total_page
+            if ($current_page > $total_page){
+                $current_page = $total_page;
+            }
+            else if ($current_page < 1){
+                $current_page = 1;
+            }
+
+            // Tìm Start
+            $start = ($current_page - 1) * $limit;
+
+            // BƯỚC 5: TRUY VẤN LẤY DANH SÁCH TIN TỨC
+            // Có limit và start rồi thì truy vấn CSDL lấy danh sách tin tức
+            $result = mysqli_query($conn, "SELECT * FROM products LIMIT $start, $limit");
+            while($row = mysqli_fetch_assoc($result)) {?>
+              <div class="col-lg-3 col-md-6 ndct">
+                <div class="card h-100">
+                  <a href="single.php?product_id=<?php echo $row["id"];?>"><img style="padding: 2px;" class="card-img-top" src="<?php echo URL_IMAGE.$row['image'];?>" alt=""></a>
+                  <div class="card-body">
+                    <h4 class="card-title">
+                      <a href="#"><?php echo $row["name"];?></a>
+                    </h4>
+                    <h5>Price : <?php echo $row["price"];?> VNĐ</h5>
+                    <p class="card-text"><?php echo $row["description"];?>.</p>
                   </div>
-                  <?php  }
-                }
+                  <div class="card-footer">
+                    <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                  </div>
+                </div>
+              </div>
+              <?php  }
 
-              ?>
+          ?>
 
-          </div>
+        </div>
           <!-- /.row -->
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+          <?php
+            if ($current_page > 1 && $total_page > 1){ ?>
+            <li class="page-item">
+              <a class="page-link" href="index.php?page=<?php echo ($current_page-1); ?>" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span>Previous</span>
+              </a>
+            </li>
+            <?php } ?>
 
+            <?php for ($i = 1; $i <= $total_page; $i++){
+              if ($i == $current_page){ ?>
+                <li class="page-item-active"><a class="page-link" href=""><?php echo $i; ?></a></li>
+              <?php }else{ ?>
+                <li class="page-item"><a class="page-link" href="index.php?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+            <?php
+                }
+              }
+            ?>
+          <?php if ($current_page < $total_page && $total_page > 1){ ?>
+            <li class="page-item">
+              <a class="page-link" href="index.php?page=<?php echo ($current_page+1); ?>" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span>Next</span>
+              </a>
+            </li>
+          <?php } ?>
+          </ul>
+        </nav>
         </div>
         <!-- /.col-lg-9 -->
 
@@ -68,11 +121,8 @@
 
     <!-- Footer -->
     <?php include "footer.php" ?>
-
     <!-- Bootstrap core JavaScript -->
-    
-
-  </body>
+</body>
 
 </html>
-<?php unset($_SESSION["flash"]); ?>
+<?php unset($_SESSION["flash"]) ?>
